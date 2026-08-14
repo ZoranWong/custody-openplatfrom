77751c9e6f6a17486389d5268b4e5e754649eb0c
minec <skyandsunning@gml.com>
2026-08-15 01:32:19 +0800

    feat(admin): add route modules for developer, KYB, ISV, stats with i18n

--- openplatform-web/admin-portal-v2/src/router/modules/index.ts
+++ openplatform-web/admin-portal-v2/src/router/modules/index.ts
@@ -3,12 +3,20 @@ import { dashboardRoutes } from './dashboard'
 import { systemRoutes } from './system'
 import { resultRoutes } from './result'
 import { exceptionRoutes } from './exception'
+import { developerRoutes } from './developer'
+import { kybRoutes } from './kyb'
+import { isvRoutes } from './isv'
+import { statsRoutes } from './stats'
 
 /**
  * 导出所有模块化路由
  */
 export const routeModules: AppRouteRecord[] = [
   dashboardRoutes,
+  developerRoutes,
+  kybRoutes,
+  isvRoutes,
+  statsRoutes,
   systemRoutes,
   resultRoutes,
   exceptionRoutes
